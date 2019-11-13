9fbf15ca3fd146dfd13bdce08dc20e3b6dd88aae
MuhammadSafwan <safwan_warind@hotmail.com>
2019-11-13 16:46:53 +0100

    Initial commit

---
+++ app/src/main/java/com/example/recyclerview/MoviesAdapter.java
@@ -0,0 +1,49 @@
+package com.example.recyclerview;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MyViewHolder> {
+
+    private List<Movie> moviesList;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView title;
+
+        public MyViewHolder(View view) {
+            super(view);
+            title = (TextView) view.findViewById(R.id.title);
+
+        }
+    }
+
+    public MoviesAdapter(List<Movie> moviesList) {
+        this.moviesList = moviesList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recyclerview_row, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        Movie movie = moviesList.get(position);
+        holder.title.setText(movie.getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return moviesList.size();
+    }
+
+}
